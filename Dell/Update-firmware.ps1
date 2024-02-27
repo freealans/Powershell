da5621e0ae02fc5b90b4d91606333f491b20901d
Alan Rossy <Alan.Rossy@synergis.com>
2024-02-27 17:00:04 -0500

    initial commit

---
+++ Dell/Update-firmware.ps1
@@ -0,0 +1,26 @@
+Update-DellFirmware {
+	<#
+	.SYNOPSIS
+	Will query and install Dell updates using Dell command Update.
+	.DESCRIPTION
+	This commnad will check to see that Dell command is installed on the system(s), then query and install udpates.
+	.PARAMETER Computername
+	One or more computernames or IP addresses to query.
+	.PARAMETER LogErrors
+	Specify this switch to create a text log file of computers that can't be queried.
+	.PARAMETER ErrorLog
+	When used with -LogErrors, specifies the file path and the name to which failed computer names will be writen.  Defaults to %userprofile%\Desktop\DellFirmwareUpgrade.txt.
+	.EXAMPLE
+	Update-DellFirmware -computername localhost
+	#>
+		[CmdletBinding()]
+		param(
+			[Parameter (Mandatory=$True, ValueFromPipeline=$True, HelpMessage="Computername or IP address")]
+			[Alias('Hostname')]
+			[string[]]$Computername,
+
+			[string]$ErrorLog = '%userprofile%\Desktop\DellFirmwareUpgrade.txt',
+
+			[switch]$LogErrors
+		)
+}
